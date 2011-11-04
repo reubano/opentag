f90f6eff3e2251137eff7d31399564dc13512fb8
Reuben Cummings <reubano@gmail.com>
2011-11-04 09:31:41 +0300

    Fix program name and add options

--- README.md
+++ README.md
@@ -1,31 +1,31 @@
-# someprogram
+# opentag
 
 ## LICENSE:
 
-This file is part of someprogram.
+This file is part of opentag.
 
-someprogram is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. 
+opentag is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. 
 
-someprogram is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+opentag is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License along with this program. If not, see [GNU.](http://www.gnu.org/licenses/)
 
-Copyright © 2010 [Reuben Cummings](mailto:reubano@gmail.com?subject=someprogram)
+Copyright © 2010 [Reuben Cummings](mailto:reubano@gmail.com?subject=opentag)
 
 ## INTRODUCTION:
 
-[someprogram](http://github.com/reubano/someprogram) is a command line program that converts CSV files to GPX files for importing into GPSPhotoLinker or similar geotagging programs. "someprogram](http://github.com/reubano/someprogram is distributed under the terms of the GNU General Public License (GPL) and has been tested on the following configuration:
+[opentag](http://github.com/reubano/opentag) is a command line program that converts CSV files to GPX files for importing into GPSPhotoLinker or similar geotagging programs. "opentag](http://github.com/reubano/opentag is distributed under the terms of the GNU General Public License (GPL) and has been tested on the following configuration:
 * MacOS X 10.5.8
 * PHP 5.3.0
 * Console_CommandLine 1.1.1
 
 ## REQUIREMENTS:
 
-[someprogram](http://github.com/reubano/someprogram) requires the following programs in order to run properly:
+[opentag](http://github.com/reubano/opentag) requires the following programs in order to run properly:
 * [PHP 5.3+](http://pear.php.net/manual/en/installation.php)
 * [PEAR](http://us2.php.net/downloads.php)
 * [Console_CommandLine](http://pear.php.net/package/Console_CommandLine/download)
-* [reubano/someprogram](http://github.com/reubano/someprogram)
+* [reubano/opentag](http://github.com/reubano/opentag)
 
 ## PREPARATION:
 
@@ -38,25 +38,25 @@ _set the directory that you wish to hold the program_
 
 	cd /path/to/repos
 
-_clone someprogram using [Git](http://git-scm.com/download) (recommended)_
+_clone opentag using [Git](http://git-scm.com/download) (recommended)_
 
 	git clone git://github.com/reubano/library.git
-	git clone git://github.com/reubano/someprogram.git
+	git clone git://github.com/reubano/opentag.git
 
 ## INSTALLATION:
 
 
-## USING someprogram:
+## USING opentag:
 
 ### Usage:
 	#!/usr/bin/env php
 	<?php
 	try {
-		require_once 'someprogram.inc.php';
+		require_once 'opentag.inc.php';
 		$time = time();
-		$imageObj = new someprogram('src', 1000, 600);
+		$imageObj = new opentag('src', 1000, 600);
 		$imageObj->execute();
-		$imageObj->saveFile("someprogram/$time.png");
+		$imageObj->saveFile("opentag/$time.png");
 		echo "done!\n";
 		exit(0);
 	} catch (Exception $e) {
--- opentag.php
+++ opentag.php
@@ -9,51 +9,60 @@
 $thisProjectDir		= dirname(__FILE__);
 $timeStamp			= date("mdy").'_'.date("His"); // format to mmddyy_hhmmss
 $stdin 				= FALSE;
-$intOption 			= 1;
-$strOption 			= 'string';
-$ext 				= 'png';
+$prefix 			= '&';
+$allFiles			= '';
+$addTag				= '';
+$setTag				= '';
+$rate				= 0;
 
 // include files
 require_once 'Console/CommandLine.php';
 require_once $thisProjectDir.'/lib_general/General.inc.php';
 
 // create the parser from xml file
-$xmlFile = $thisProjectDir.'/someprogram.xml';
+$xmlFile = $thisProjectDir.'/opentag.xml';
 $parser = Console_CommandLine::fromxmlFile($xmlFile);
 
 try {	
 	// run the parser
 	$result = $parser->parse();
 
-	// command arguments
-	$input = $result->args['input'];
+	// command argument
+	$file = $result->args['file'];
 	
-	if ($input == '$') {
+	if ($file == '$') {
 		$stdin = TRUE;
-	} //<-- end if -->
-
-	if ($result->args['output']) {
-		$output = $result->args['output'];
-	} else {
-		$output = $thisProjectDir.'/export/chart'.'_'.$timeStamp.'.'.$ext;
 	} //<-- end if -->
 	
 	// command options
 	$debugmode	= $result->options['debug'];
 	$varmode	= $result->options['variables'];
 	$verbose	= $result->options['verbose'];
-
+	$rating		= $result->options['rating'];
+	$spotlight	= $result->options['spotlight'];
+	$tags		= $result->options['tags'];
+	$untag		= $result->options['untag'];
+	$unrate		= $result->options['unrate'];
+	
 	// program setting
 	$general = new general($verbose);
 	$program = $general->getBase(__FILE__);
 	
 	// load options if present
-	if ($result->options['intOption']) {
-		$intOption = $result->options['intOption'];
+	if ($result->options['prefix']) {
+		$prefix = $result->options['prefix'];
 	} //<-- end if -->
 	
-	if ($result->options['strOption']) {
-		$strOption = $result->options['strOption'];
+	if ($result->options['rate']) {
+		$rate = $result->options['rate'];
+	} //<-- end if -->
+	
+	if ($result->options['addTag']) {
+		$addTag = $result->options['addTag'];
+	} //<-- end if -->
+	
+	if ($result->options['setTag']) {
+		$setTag = $result->options['setTag'];
 	} //<-- end if -->
 
 	// debug and variable mode settings
@@ -74,9 +83,71 @@ try {
 
 	// execute program
 	if($stdin) {
-		$input = $general->readSTDIN();
+		$file = $general->readSTDIN();
+		$file = explode("\n", $file); // turn string to array
+		array_pop($file); // remove last element since it is empty
 	} //<-- end if -->
+			
+	foreach ($file as $key => $value) {
+		if (strpos($value, '/') === FALSE) {
+			$file[$key] = '"'.exec("pwd").'/'.$value.'"';
+		} //<-- end if -->
+	} //<-- end foreach -->
 
+	foreach ($file as $aFile) {
+		$allFiles .= $aFile.' ';
+	} //<-- end foreach -->
+	
+	$allFiles = trim($allFiles);
+	
+	if ($addTag || $setTag) {
+		$theTags = str_replace(',', ' ', $addTag);
+		
+		if ($addTag) {
+			exec("openmeta -a $theTags -p $allFiles");
+		} else {
+			exec("openmeta -s $theTags -p $allFiles");
+		} //<-- end if -->
+	} //<-- end if -->
+	
+	if ($untag) {
+		exec("openmeta -s -p $allFiles");
+	} //<-- end if -->
+	
+	if ($tags) {
+		exec("mdls -name kMDItemOMUserTags -raw $allFiles", $output);
+		array_pop($output);
+		array_shift($output);
+		foreach 
+		print_r($output);
+		fwrite(STDOUT, "openmeta: \n");
+
+		if ($spotlight) {
+			$comment = exec("mdls -name kMDItemFinderComment -raw $allFiles");	
+			$comment = explode(' ', $comment);
+			
+			foreach ($comment as $chunk) {
+				if (strpos($chunk, $prefix) === 0) {
+					$tags .= str_replace($prefix, '', $chunk).' ';
+				} //<-- end if -->
+			} //<-- end foreach -->
+			
+			fwrite(STDOUT, "spotlight: $tags\n");
+		} //<-- end if -->
+	} //<-- end if -->
+	
+	if ($rate) {
+		exec("openmeta -r $rate -p $allFiles");
+	} //<-- end if -->
+	
+	if ($unrate) {
+		exec("openmeta -r 0 -p $allFiles");
+	} //<-- end if -->
+	
+	if ($rating) {
+		exec("openmeta -r -p $allFiles");
+	} //<-- end if -->
+	
 	exit(0);
 } catch (Exception $e) {
 	fwrite(STDOUT, 'Program '.$program.': '.$e->getMessage()."\n");
